aa951c4d3c38c35456d8b8bace1af9175093721e
Ramil Vildanov <ramvildan.work@gmail.com>
2024-06-25 00:12:54 +0300

    WIP: Added primitive check and array check, class Man

--- src/util/CopyUtils.java
+++ src/util/CopyUtils.java
@@ -1,6 +1,16 @@
 package util;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CopyUtils {
 
@@ -15,6 +25,29 @@ public class CopyUtils {
     }
 
     private static Object recursiveCopy(Object origObject, IdentityHashMap<Object, Object> visitedObjectMap) {
+        if (origObject == null || origObject.getClass().isPrimitive() ||
+                origObject.getClass().isEnum() || origObject instanceof String) {
+
+            return origObject;
+        }
+
+        if (visitedObjectMap.containsKey(origObject)) {
+
+            return visitedObjectMap.get(origObject);
+        }
+
+        if (origObject.getClass().isArray()) {
+            int arrayLength = Array.getLength(origObject);
+
+            Object arrayCopy = Array.newInstance(origObject.getClass().getComponentType(), arrayLength);
+            visitedObjectMap.put(origObject, arrayCopy);
+
+            for (int i = 0; i < arrayLength; i++) {
+                Array.set(arrayCopy, i, recursiveCopy(Array.get(origObject, i), visitedObjectMap));
+            }
+
+            return arrayCopy;
+        }
         return null;
     }
 }
